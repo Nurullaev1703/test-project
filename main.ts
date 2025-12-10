01411a8c4369f4331efce7de3f0769db6cf54751
DiDenPro <93188632+DiDenPro@users.noreply.github.com>
2025-12-10 19:59:12 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+import { data } from "./src/scripts/data";
+
+const dataAsArray = Object.entries(data)
+
+let maxIncomeSeason = dataAsArray[0];
+
+dataAsArray.forEach(([season, info]) => {
+    // ищем самый прибыльный сезон
+    if(info.income > maxIncomeSeason[1].income) {
+        maxIncomeSeason = [season, info];
+    }
+});
+console.log(maxIncomeSeason);
+// console.log("Доход в этом сезоне:", maxIncomeSeason[1].income);
---
+++ src/scripts/data.ts
@@ -0,0 +1,31 @@
+export const data = {
+  winter: {
+    closeProjects: 10,
+    income: 600000,
+    hired: 10,
+    dismissed: 10,
+    newProjects: 10,
+  },
+  spring: {
+    closeProjects: 10,
+    income: 7500000,
+    hired: 10,
+    dismissed: 10,
+    newProjects: 10,
+  },
+  summer: {
+    closeProjects: 10,
+    income: 550000,
+    hired: 10,
+    dismissed: 10,
+    newProjects: 10,
+  },
+  autumn: {
+    closeProjects: 10,
+    income: 900000,
+    hired: 10,
+    dismissed: 10,
+    newProjects: 10,
+  },
+};
+
